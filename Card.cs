7d7d8813e552904c8edbb4bd40c2bf223afb8b9f
YuseinToslev <jusein.toslev.highschool@svetlina.net>
2023-12-07 21:52:10 +0200

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfWar
+{
+    public class Card
+    {
+        public CardFace Face { get; set; }
+        public CardSuit Suite { get; set; }
+
+        public override string ToString()
+        {
+            if ((int)Enum.Parse(typeof(CardFace), this.Face.ToString()) > 10)
+            {
+                return $"{this.Face}{(char)this.Suite}";
+            }
+
+            return $"{(int)Enum.Parse(typeof(CardFace), this.Face.ToString())}{(char)this.Suite}";
+        }
+    }
+}
